5484db100bbed876c4c1cea8100a61dfe5b5295f
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2016-05-06 14:49:54 +0300

    Attempt to fix not loading static files

--- MyInstagram/settings.py
+++ MyInstagram/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'MyInstagram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(_PATH, 'templates'), ],
+        'DIRS': [os.path.join(PROJECT_ROOT, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
